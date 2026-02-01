b16904f476e8b846b773f6d25c3b091ce6e96c68
Brajesh <prajapatibrajesh003@gmail.com>
2026-02-01 23:49:37 +0530

    initial commit

---
+++ devsolver-backend/src/main/java/com/major/devsolver_backend/DevsolverBackendApplication.java
@@ -0,0 +1,13 @@
+package com.major.devsolver_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevsolverBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevsolverBackendApplication.class, args);
+	}
+
+}
---
+++ devsolver-backend/src/test/java/com/major/devsolver_backend/DevsolverBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.major.devsolver_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevsolverBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
