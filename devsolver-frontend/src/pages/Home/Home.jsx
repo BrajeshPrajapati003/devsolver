fc39e4e7e7b09334da8c83a0737c39ff7a9aff4d
sinran <ankitsinghaa3@gmai.com>
2026-07-31 17:10:29 +0530

    Create MainLayout to include Navbar and Footer

--- devsolver-frontend/src/pages/Home/Home.jsx
+++ devsolver-frontend/src/pages/Home/Home.jsx
@@ -1,21 +1,22 @@
-import Navbar from "../../components/layout/Navbar/Navbar";
+// import Navbar from "../../components/layout/Navbar/Navbar";
 import Hero from "../../components/home/Hero/Hero";
 import Features from "../../components/Home/Features/Features";
 import HowItWorks from "../../components/Home/HowItWorks/HowItWorks";
 import Stats from "../../components/Home/Stats/Stats";
 import CTA from "../../components/Home/CTA/CTA";
-import Footer from "../../components/layout/Footer/Footer";
+// import Footer from "../../components/layout/Footer/Footer";
+import MainLayout from "../../components/layout/MainLayout/MainLayout";
 
 const Home = () => {
   return (
     <>
-      <Navbar />
+      <MainLayout>
       <Hero />
       <Features />
       <HowItWorks />
       <Stats />
       <CTA />
-      <Footer />
+      </ MainLayout>
     </>
   );
 };
