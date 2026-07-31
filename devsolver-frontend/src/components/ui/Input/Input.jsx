ac542046451d5722ecb95c1316ceb9a99d861f92
sinran <ankitsinghaa3@gmai.com>
2026-07-31 17:13:26 +0530

    Create input component

---
+++ devsolver-frontend/src/components/ui/Input/Input.jsx
@@ -0,0 +1,28 @@
+import "./Input.css";
+
+const Input = ({
+  label,
+  type = "text",
+  placeholder,
+  value,
+  onChange,
+  name,
+}) => {
+  return (
+    <div className="input-group">
+
+      <label>{label}</label>
+
+      <input
+        type={type}
+        name={name}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+      />
+
+    </div>
+  );
+};
+
+export default Input;
